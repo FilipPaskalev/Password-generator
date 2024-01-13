a2723853008934bb6338bf81019b61ae1a9e9dd1
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-01-13 22:11:15 +0000

    correct checking in generatePassword() to make sure that at least one char from selected will be in password

--- script.js
+++ script.js
@@ -60,10 +60,10 @@ function generatePassword() {
   var options = getPasswordOptions();
   var password = "";
 
-  if (options.inclSpecialChars) password = password.concat(utils.chars.special);
-  if (options.inclNumbers) password = password.concat(utils.chars.numeric);
-  if (options.inclLowerCase) password = password.concat(utils.chars.lowerCased);
-  if (options.inclUpperCase) password = password.concat(utils.chars.upperCase);
+  if (options.inclSpecialChars) password = password.concat(getRandom(utils.chars.special));
+  if (options.inclNumbers) password = password.concat(getRandom(utils.chars.numeric));
+  if (options.inclLowerCase) password = password.concat(getRandom(utils.chars.lowerCased));
+  if (options.inclUpperCase) password = password.concat(getRandom(utils.chars.upperCase));
 
   return password;
 }
