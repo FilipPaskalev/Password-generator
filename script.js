75c9dac2479e51781bea935c17da294e56c6b095
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-01-13 22:47:08 +0000

    refactor object utils.chars to contain typeName. Transform chars in array

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 // prettier-ignore
 const utils = {
-  chars: {
-    special:["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", "."],
-    numeric:["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-    lowerCased: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-    upperCase:["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-  },
+  chars: [
+    {
+      typeName: "special symbols",
+      special: ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", "."],
+    },
+    {
+      typeName: "number characters",
+      numeric:["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    },
+    {
+      typeName: "lower case letters",
+      lowerCased: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    },
+    {
+      typeName: "upper case letters",
+      upperCase:["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+    }
+  ],
   length: {
     min: 8,
     max: 128,
@@ -15,7 +27,7 @@ const utils = {
     optionsCondition: "Select at least one option for generating the password.",
     enterLength: (min, max) => `Enter length of the password. It should be a number between ${min} and ${max}`,
     wrongLength: (min, max, startAgainMsg) => `Incorrect password length. Please enter a valid password length between, it should be number between ${min} and ${max}.\n\n${startAgainMsg}`,
-    selectOption: (characterName, type, condition) => `Select if you want to include ${characterName} ${type} in password.\n${condition}`,
+    selectOption: (typeName, condition) => `Select if you want to include ${typeName} in password.\n${condition}`,
     noneSelectedOptions: (condition, startAgainMsg) => `None of the options have been selected. ${condition}\n\n${startAgainMsg}`,
   }
 }
@@ -37,10 +49,10 @@ function getPasswordOptions() {
     return;
   }
 
-  options.inclSpecialChars = confirm(utils.msgs.selectOption("special", "symbols", utils.msgs.optionsCondition));
-  options.inclNumbers = confirm(utils.msgs.selectOption("number", "characters", utils.msgs.optionsCondition));
-  options.inclLowerCase = confirm(utils.msgs.selectOption("lower case", "letters", utils.msgs.optionsCondition));
-  options.inclUpperCase = confirm(utils.msgs.selectOption("upper case", "letters", utils.msgs.optionsCondition));
+  options.inclSpecialChars = confirm(utils.msgs.selectOption(utils.chars[0].typeName, utils.msgs.optionsCondition));
+  options.inclNumbers = confirm(utils.msgs.selectOption(utils.chars[1].typeName, utils.msgs.optionsCondition));
+  options.inclLowerCase = confirm(utils.msgs.selectOption(utils.chars[2].typeName, utils.msgs.optionsCondition));
+  options.inclUpperCase = confirm(utils.msgs.selectOption(utils.chars[3].typeName, utils.msgs.optionsCondition));
 
   if (!options.inclSpecialChars && !options.inclNumbers && !options.inclLowerCase && !options.inclUpperCase) {
     alert(utils.msgs.noneSelectedOptions(utils.msgs.optionsCondition, utils.msgs.startAgain));
@@ -61,17 +73,17 @@ function generatePassword() {
 
   var password = "";
 
-  if (options.inclSpecialChars) password = password.concat(getRandom(utils.chars.special));
-  if (options.inclNumbers) password = password.concat(getRandom(utils.chars.numeric));
-  if (options.inclLowerCase) password = password.concat(getRandom(utils.chars.lowerCased));
-  if (options.inclUpperCase) password = password.concat(getRandom(utils.chars.upperCase));
+  if (options.inclSpecialChars) password = password.concat(getRandom(utils.chars[0].special));
+  if (options.inclNumbers) password = password.concat(getRandom(utils.chars[1].numeric));
+  if (options.inclLowerCase) password = password.concat(getRandom(utils.chars[2].lowerCased));
+  if (options.inclUpperCase) password = password.concat(getRandom(utils.chars[3].upperCase));
 
   while (password.length < options.length) {
     var randomChar = getRandom([
-      utils.chars.special,
-      utils.chars.numeric,
-      utils.chars.lowerCased,
-      utils.chars.upperCase,
+      utils.chars[0].special,
+      utils.chars[1].numeric,
+      utils.chars[2].lowerCased,
+      utils.chars[3].upperCase,
     ]);
     password += getRandom(randomChar);
   }
