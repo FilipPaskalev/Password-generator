bf0877b0b7294b8394bd01c59819b9c744148eef
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-01-13 22:17:23 +0000

    refactor in obj utils.msgs for clear meaning message

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ const utils = {
   },
   msgs: {
     startAgain: "Please try again to generate password.",
-    optionsCondition: "Select at least one option for the password.",
+    optionsCondition: "Select at least one option for generating the password.",
     enterLength: (min, max) => `Enter length of the password. It should be a number between ${min} and ${max}`,
     wrongLength: (min, max, startAgainMsg) => `Incorrect password length. Please enter a valid password length between, it should be number between ${min} and ${max}.\n\n${startAgainMsg}`,
     selectOption: (characterName, type, condition) => `Select if you want to include ${characterName} ${type} in password.\n${condition}`,
-    noneSelectedOptions: (condition, startAgainMsg) => `None options have been selected. ${condition}\n\n${startAgainMsg}`,
+    noneSelectedOptions: (condition, startAgainMsg) => `None of the options have been selected. ${condition}\n\n${startAgainMsg}`,
   }
 }
 
